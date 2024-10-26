3ed19cdc067e7d92d3e9763dd3c3058aca6b0bec
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2024-10-26 18:53:41 +0330

    feat: implement base of database config and env

---
+++ src/database.py
@@ -0,0 +1,22 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from config import Settings
+
+Base = declarative_base()
+settings = Settings()
+
+
+engine = create_async_engine(settings.database_url, echo=settings.debug)
+
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
