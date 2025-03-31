2c254a7a2fedf33eff8bb25fe78287fe5a43522e
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2025-03-31 20:18:29 +0330

    feat: replace jti with hashed jti

--- src/models/jwt.py
+++ src/models/jwt.py
@@ -1,19 +1,17 @@
-import hashlib
 from datetime import datetime
-from sqlalchemy import DateTime, ForeignKey, func, Index
+
+from sqlalchemy import DateTime, ForeignKey, Index, func
 from sqlalchemy.orm import Mapped, mapped_column
+
 from src.configs.database import Base
 from src.models.user import User
 
-# def hash_jti(jti: str) -> str:
-#     """Hash the jti for storage in the database."""
-#     return hashlib.sha256(jti.encode('utf-8')).hexdigest()
 
 class BlackListRefreshToken(Base):
     __tablename__ = "blacklist_refresh_tokens"
 
     id: Mapped[int] = mapped_column(
-        primary_key=True
+        primary_key=True,
     )
     user_id: Mapped[int] = mapped_column(
         ForeignKey(User.id, ondelete="CASCADE"),
@@ -26,12 +24,8 @@ class BlackListRefreshToken(Base):
         default=False,
     )
 
-    hashed_jti: Mapped[str] = mapped_column()
+    jti: Mapped[str] = mapped_column()
 
     __table_args__ = (
-        Index("idx_user_hashed_jti", "user_id", "hashed_jti"),  
+        Index("idx_user_jti", "user_id", "jti", postgresql_using="hash"),  
     )
-
-    def revoke(self):
-        """Mark this token as revoked."""
-        self.revoked = True
