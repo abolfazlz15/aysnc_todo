9a207e8eee037aa18d65a044e432626d1261c280
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2025-04-14 14:47:36 +0330

    feat: add get task endpoint

--- src/repositories/task_repository.py
+++ src/repositories/task_repository.py
@@ -8,11 +8,11 @@ class TaskRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_task_by_id(self, task_id: int):
-        result = await self.session.scalar(sa.select(Task).where(Task.id == task_id))
-        if result is None:
+    async def get_task_by_id(self, task_id: int, user_id: int) -> Task | None:
+        task_obj = await self.session.scalar(sa.select(Task).where(Task.id == task_id, Task.user_id == user_id))
+        if task_obj is None:
             return None
-        return result
+        return task_obj
 
     async def create_task(self, title: str, content: str, user_id: int, status: bool = True) -> Task:
         new_task = Task(title=title, content=content, user_id=user_id, status=status)
--- src/routers/task.py
+++ src/routers/task.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.configs.database import get_db
@@ -19,13 +20,23 @@ async def create_task_router(
     current_user: UserInDBSchema = Depends(get_current_active_user),
     db: AsyncSession = Depends(get_db),
 ) -> None:
-    """
-    Create a new task.
-    """
     try:
         return await TaskRepository(db).create_task(**task.model_dump(), user_id=current_user.id)
-    except Exception as e:
+    except IntegrityError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Failed to create task: {str(e)}",
+            detail="something went wrong",
         )
+    
+@router.get("/get/{task_id}/", response_model=TaskCreateOutSchema, name="task:get")
+async def get_task_router(
+    task_id: int,
+    current_user: UserInDBSchema = Depends(get_current_active_user),
+    db: AsyncSession = Depends(get_db),
+) -> None:
+    if task := await TaskRepository(db).get_task_by_id(task_id=task_id, user_id=current_user.id):
+        return task
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="task not found",
+    )
